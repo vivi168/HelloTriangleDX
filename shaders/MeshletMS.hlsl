4c31f3c45e497e6b407db974474647b4fdf4b982
vivi168 <vivienbihl@gmail.com>
2025-07-12 13:07:39 +0900

    Triangle culling

--- shaders/MeshletMS.hlsl
+++ shaders/MeshletMS.hlsl
@@ -49,11 +49,17 @@ MS_OUTPUT GetVertexAttributes(MeshInstance mi, uint meshletIndex, uint vertexInd
   return vout;
 }
 
+float2 ClipToUv(float2 clip) { return clip * 0.5 + 0.5; }
+
+float2 ClipToScreen(float2 clip, float2 screen) { return ClipToUv(clip) * screen; }
+
 struct PRIM_OUT
 {
   uint PrimitiveId : SV_PrimitiveID;
   bool culled : SV_CullPrimitive;
 };
+
+groupshared float3 s_PositionsCS[MESHLET_MAX_VERT];
 
 [NumThreads(128, 1, 1)]
 [OutputTopology("triangle")]
@@ -77,16 +83,45 @@ void main(
 
   SetMeshOutputCounts(m.VertCount, m.PrimCount);
 
-  if (gtid < m.PrimCount)
-  {
-    tris[gtid] = GetPrimitive(m, mi.primBufferOffset + gtid);
-    prims[gtid].PrimitiveId = gtid;
-    prims[gtid].culled = false;
-  }
-
   if (gtid < m.VertCount)
   {
     uint vertexIndex = GetVertexIndex(m, mi.indexBufferOffset + gtid);
-    verts[gtid] = GetVertexAttributes(mi, gid, vertexIndex, m.materialIndex);
+    MS_OUTPUT v = GetVertexAttributes(mi, gid, vertexIndex, m.materialIndex);
+    verts[gtid] = v;
+    s_PositionsCS[gtid] = float3(ClipToScreen(v.posCS.xy / v.posCS.w, ScreenSize), v.posCS.w);
+  }
+
+  GroupMemoryBarrierWithGroupSync();
+
+  if (gtid < m.PrimCount)
+  {
+    uint3 tri = GetPrimitive(m, mi.primBufferOffset + gtid);
+    tris[gtid] = tri;
+
+    bool culled = false;
+
+    // https://github.com/zeux/niagara
+    // Backface culling + zero area culling
+    float2 pa = s_PositionsCS[tri.x].xy;
+    float2 pb = s_PositionsCS[tri.y].xy;
+    float2 pc = s_PositionsCS[tri.z].xy;
+
+    float2 eb = pb - pa;
+    float2 ec = pc - pa;
+
+    culled = culled || (eb.x * ec.y <= eb.y * ec.x);
+
+    float2 bmin = min(pa, min(pb, pc));
+    float2 bmax = max(pa, max(pb, pc));
+    float sbprec = 1.0 / 256.0;  // note: this can be set to 1/2^subpixelPrecisionBits
+
+    // Note: this is slightly imprecise (doesn't fully match hw behavior and is both too loose and too strict)
+    culled = culled || (round(bmin.x - sbprec) == round(bmax.x) || round(bmin.y) == round(bmax.y + sbprec));
+
+    // Above computations valid only if all vertices are in front of perspective plane
+    culled = culled && (s_PositionsCS[tri.x].z > 0 && s_PositionsCS[tri.y].z > 0 && s_PositionsCS[tri.z].z > 0);
+
+    prims[gtid].PrimitiveId = gtid;
+    prims[gtid].culled = culled;
   }
 }
