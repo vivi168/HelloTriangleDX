d347b39b1267df0d8a0bcf29d480c7c7391b5c89
vivi168 <vivienbihl@gmail.com>
2025-07-25 01:28:13 +0900

    texture alpha test condition

--- shaders/MeshletPS.hlsl
+++ shaders/MeshletPS.hlsl
@@ -7,11 +7,12 @@ SamplerState s1 : register(s1);
 
 uint main(VertexOut v, uint primitiveIndex : SV_PrimitiveID) : SV_Target
 {
+// #define ENABLE_TEXTURE_ALPHA_TEST
 #ifdef ENABLE_TEXTURE_ALPHA_TEST
   Texture2D baseColorTex = ResourceDescriptorHeap[NonUniformResourceIndex(v.textureIndex)];
   float4 baseColor = baseColorTex.Sample(s1, v.uv);
 
-  if (baseColor.w == 0) discard;
+  if (baseColor.w < 0.1) discard;
 #endif
   return PackVisibility(v.meshletIndex, primitiveIndex);
 }
